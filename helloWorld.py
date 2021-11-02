a315d594e0341aca874b10290118b5de221f06c0
Tozarf <fazatojuni@gmail.com>
2021-11-02 16:23:26 -0600

    solution with %

--- helloWorld.py
+++ helloWorld.py
@@ -2,6 +2,8 @@
 # print (var)  
 
 pruebapares = 0
-while pruebapares % 2 and pruebapares <101:
+comparison= pruebapares % 2
+
+while comparison == 0 and pruebapares <101:
     print(pruebapares)
     pruebapares += 1
